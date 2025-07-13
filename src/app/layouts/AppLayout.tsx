11d076d61979977a87b6c7a64690acbbe89e06e5
Nick G <nolimit48@yandex.ru>
2025-07-13 17:40:37 +0200

    update

--- src/app/layouts/AppLayout.tsx
+++ src/app/layouts/AppLayout.tsx
@@ -1,10 +1,12 @@
 import Header from '../components/Header.tsx';
+import Footer from '../components/Footer.tsx';
 
 export const AppLayout = ({ children }) => {
   return (
     <>
       <Header />
       <main className="main-container">{children}</main>
+      <Footer />
     </>
   );
 };
