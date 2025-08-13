36006d65c1d36334e3f38dde4cdb7f5ca8d10812
Nick G <nolimit48@yandex.ru>
2025-08-13 20:27:05 +0200

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import { routes } from './routes.tsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <Router>
+      <ToastContainer />
       <Routes>
         {routes.map(({ path, element }) => (
           <Route key={path} path={path} element={element} />
--- src/app/pages/MainPage.tsx
+++ src/app/pages/MainPage.tsx
@@ -1,10 +1,20 @@
+import { useEffect } from 'react';
 import MovieSearchInput from '@/app/components/MovieSearchInput.tsx';
 import MovieResult from '@/app/components/MovieResults.tsx';
 import Loader from '@/app/features/Loader.jsx';
 import { useLazySearchMoviesQuery } from '@/api/moviesApi.ts';
+import { toast } from 'react-toastify';
 
 const MainPage = () => {
   const [trigger, { data, isFetching, error }] = useLazySearchMoviesQuery();
+
+  useEffect(() => {
+    if (!error) return;
+    toast('Ошибка сети', {
+      type: 'error',
+      theme: 'dark',
+    });
+  }, [error]);
 
   return (
     <>
